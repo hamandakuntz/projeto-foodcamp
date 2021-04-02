4531da2e1d6cacfab4b30fb414b9976a563bc997
Hamanda Kuntz <hamanda.monteiro@hotmail.com>
2021-04-02 13:29:07 -0300

    feature seleção de itens finalizada

--- scripts.js
+++ scripts.js
@@ -1,32 +1,59 @@
-function selecionarPrato1 () {
-    const pratoSelecionado1 = document.querySelector(".pratos");            
-    pratoSelecionado1.classList.add("selecionado"); 
-               
-    const iconSelecionado1 = document.querySelector("ion-icon");            
-    iconSelecionado1.classList.remove("escondido");
+
+let prato;
+
+function selecionarPrato(classePrato, nomePrato) {
+    prato = nomePrato;
+    const selecionado = document.querySelector(".lista-pratos .selecionado");        
+    
+    if(selecionado !== null) {
+        selecionado.classList.remove('selecionado'); 
+        const icone = selecionado.querySelector("ion-icon");
+        icone.classList.add('escondido'); 
+    }
+    
+    const seletor = "." + classePrato;    
+    const pratoSelecionado = document.querySelector(seletor);
+    pratoSelecionado.classList.add('selecionado');  
+    const icone = pratoSelecionado.querySelector("ion-icon");
+    icone.classList.remove("escondido");   
 }    
 
-    
-function selecionarPrato2 () {
-    const pratoSelecionado2 = document.querySelector(".pratos:nth-child(2)");            
-    pratoSelecionado2.classList.add("selecionado"); 
-            
-    const iconSelecionado2 = document.querySelector(".pratos:nth-child(2) ion-icon");            
-    iconSelecionado2.classList.remove("escondido"); 
-}
-    
+let bebida;
 
+function selecionarBebida(classeBebida, nomeBebida) {
+    bebida = nomeBebida;
+    const selecionado = document.querySelector(".lista-bebidas .selecionado");        
+    
+    if(selecionado !== null) {
+        selecionado.classList.remove('selecionado'); 
+        const icone = selecionado.querySelector("ion-icon");
+        icone.classList.add('escondido'); 
+    }
+    
+    const seletor = "." + classeBebida;    
+    const bebidaSelecionada = document.querySelector(seletor);
+    bebidaSelecionada.classList.add('selecionado');  
+    const icone = bebidaSelecionada.querySelector("ion-icon");
+    icone.classList.remove("escondido");   
+}   
 
-function selecionarPrato3 () {
-    const pratoSelecionado3 = document.querySelector(".pratos:nth-child(3)");            
-    pratoSelecionado3.classList.add("selecionado"); 
-                   
-    const iconSelecionado3 = document.querySelector(".pratos:nth-child(3) ion-icon");            
-    iconSelecionado3.classList.remove("escondido");
-}
-         
-          
-   
-       
+let sobremesa;
+
+function selecionarSobremesa(classeSobremesa, nomeSobremesa) {
+    sobremesa = nomeSobremesa;
+    const selecionado = document.querySelector(".lista-sobremesas .selecionado");        
+    
+    if(selecionado !== null) {
+        selecionado.classList.remove('selecionado'); 
+        const icone = selecionado.querySelector("ion-icon");
+        icone.classList.add('escondido'); 
+    }
+    
+    const seletor = "." + classeSobremesa;    
+    const sobremesaSelecionada = document.querySelector(seletor);
+    sobremesaSelecionada.classList.add('selecionado');  
+    const icone = sobremesaSelecionada.querySelector("ion-icon");
+    icone.classList.remove("escondido");   
+}      
 
 
